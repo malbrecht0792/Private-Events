8a9c48e3ec1d5fdb703282a457744420ae326a55
Marcel <malbrecht0792@gmail.com>
2017-05-25 16:58:58 -0500

    Login user upon sign up added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,8 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
   	if @user.save
+      session[:name] = @user.name
+      session[:id] = @user.id
   		redirect_to user_path(@user.id)
   	else
   		render new
